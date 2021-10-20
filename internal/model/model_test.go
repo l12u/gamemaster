6f77dcbb0e0fcb4b03ffa9d6272170c83375b826
Tobias B <germansuperioz@yahoo.de>
2021-10-20 16:47:08 +0200

    Jooo

--- internal/model/model_test.go
+++ internal/model/model_test.go
@@ -9,10 +9,10 @@ import (
 func TestIsSupportedState(t *testing.T) {
 	a := assert.New(t)
 
-	a.False(IsSupportedRole("foobar"))
-	a.False(IsSupportedRole("12467"))
-	a.True(IsSupportedRole(StateLobby))
-	a.True(IsSupportedRole(strings.ToUpper(StateRunning)))
+	a.False(IsSupportedState("foobar"))
+	a.False(IsSupportedState("12467"))
+	a.True(IsSupportedState(StateLobby))
+	a.True(IsSupportedState(strings.ToUpper(StateRunning)))
 }
 
 func TestIsSupportedRole(t *testing.T) {
